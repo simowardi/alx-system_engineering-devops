b7a6f437e0898eeabe7b0c87af0b721c5e44e25d
simowardi <daoist32@gmail.com>
2024-06-04 22:09:49 +0000

    adveced api 01

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -27,7 +27,7 @@ def number_of_subscribers(subreddit):
     if response.status_code == 200:
         data = response.json()
         subscribers = data['data']['subscribers']
-            return subscribers
+        return subscribers
 
     else:
         return 0
