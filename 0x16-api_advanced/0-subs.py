db5d8f9b6c9dfdd1fbc826b0d8344eb071574133
simowardi <daoist32@gmail.com>
2024-06-04 21:52:45 +0000

    adveced api 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,20 +10,24 @@ def number_of_subscribers(subreddit):
     """
     Function that queries the Reddit API and returns the number of subscribers
     (not active users, total subscribers) for a given subreddit.
-    If the subreddit is invalid or the API request fails, returns 0.
-    """
-    if subreddit not isinstance(subreddit, str):
-        print("None")
 
+    Args:
+    subreddit (str): The subreddit name to query.
+
+    Returns:
+    int: The number of subscribers of the subreddit.
+    """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
 
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 200:
-            data = response.json()
-            return data['data']['subscribers']
-        else:
-            return 0
-    except requests.RequestException:
+    # Make a GET request to the Reddit API
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    # Check if the request was successful (status code 200)
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
+        # Return 0 if the subreddit is invalid or request fails
         return 0
