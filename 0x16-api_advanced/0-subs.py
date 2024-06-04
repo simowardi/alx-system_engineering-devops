560935195f1fcd1f4abb44a57368eafbdd61bebe
simowardi <daoist32@gmail.com>
2024-06-04 21:30:55 +0000

    adveced api 0&1

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,28 +5,26 @@ Script to get the number of subscribers on a given Reddit subreddit.
 
 import requests
 
+
 def number_of_subscribers(subreddit):
     """
     Function that queries the Reddit API and returns the number of subscribers
     (not active users, total subscribers) for a given subreddit.
     If the subreddit is invalid, returns 0.
     """
-    # Check if the subreddit parameter is a string
     if not isinstance(subreddit, str):
         return 0
 
-    # Prepare the URL for the Reddit API request
-    api_url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    # Set a user-agent to avoid request blocking by Reddit
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
-    # Make the API request
-    response = requests.get(api_url, headers=headers, allow_redirects=False)
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
-    # If the request is successful, extract and return the subscriber count
     if response.status_code == 200:
-        data = response.json()
-        subscriber_count = data['data']['subscribers']
-        return subscriber_count
+        try:
+            data = response.json()
+            subscriber_count = data['data']['subscribers']
+            return subscriber_count
+        except (ValueError, KeyError):
+            return 0
     else:
-        # Return 0 if the request was not successful
         return 0
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 """
 Script to get the titles of the first 10 hot posts from a given subreddit.
 """
@@ -11,24 +10,25 @@ def top_ten(subreddit):
     """
     Function that queries the Reddit API and prints the titles of the first
     10 hot posts listed for a given subreddit.
+
+    Attributes:
+        subreddit (str): The target subreddit.
     """
-    # Check if the subreddit parameter is a valid string
     if subreddit is None or not isinstance(subreddit, str):
         print(None)
+        return
 
-    # Set up user agent and parameters for the API request
     user_agent = {'User-agent': 'Mozilla/5.0'}
     params = {'limit': 10}
     url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
 
-    # Make the API request
-    response = get(url, headers=user_agent, params=params, allow_redirects=False)
+    response = get(url, headers=user_agent, params=params,
+                   allow_redirects=False)
 
-    # Check if the request was successful
     if response.status_code != 200:
         print(None)
+        return
 
-    # Process the response JSON and print the titles of the posts
     try:
         data = response.json().get('data', {}).get('children', [])
         for post in data:
