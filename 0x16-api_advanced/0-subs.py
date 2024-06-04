dc15a22e100369d8a58ba226bb26507d3b8c451a
simowardi <daoist32@gmail.com>
2024-06-04 21:38:48 +0000

    adveced api 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,9 +12,7 @@ def number_of_subscribers(subreddit):
     (not active users, total subscribers) for a given subreddit.
     If the subreddit is invalid, returns 0.
     """
-    if not isinstance(subreddit, str):
-        return 0
-
+    
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
