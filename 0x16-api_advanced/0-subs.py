b8124d69df291dd4dfa9245d4b6a1d12fa59d662
simowardi <daoist32@gmail.com>
2024-06-04 21:50:32 +0000

    adveced api ~

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,16 +12,18 @@ def number_of_subscribers(subreddit):
     (not active users, total subscribers) for a given subreddit.
     If the subreddit is invalid or the API request fails, returns 0.
     """
-    if subreddit is None or not isinstance(subreddit, str):
+    if subreddit not isinstance(subreddit, str):
         print("None")
 
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
 
-    if response.status_code == 200:
-        data = response.json()
-        subscriber_count = data['data']['subscribers']
-            return subscriber_count
-    else:
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json()
+            return data['data']['subscribers']
+        else:
+            return 0
+    except requests.RequestException:
         return 0
